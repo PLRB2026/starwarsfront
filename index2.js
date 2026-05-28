dddd174f530e4eb3f529062a260198341dbedc31
PEDRO LUIS <m20.saban.20.1.1@gmail.com>
2026-05-27 21:07:50 -0500

    Initial commit

---
+++ index2.js
@@ -0,0 +1,48 @@
+const characterListDiv = document.getElementById('character-list');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+
+let prevUrl = null;
+let nextUrl = null;
+
+const CargarPersonajes = async (url) => {
+
+    const PersonajeResponse = await fetch(url);
+    const personajes = await PersonajeResponse.json();
+
+    const personajesLimitados = personajes.slice(0, 10);
+
+    characterListDiv.innerHTML = '';
+    personajesLimitados.forEach((personaje) => {
+        // sacar ID desde la URL
+        const splits = personaje.url.split('/');
+        const id = splits[splits.length - 2];
+        characterListDiv.innerHTML += `
+        <div class="bg-gray-800 rounded-2xl shadow-md p-4 hover:scale-105 hover:shadow-xl transition duration-300 cursor-pointer">
+            <img class="w-28 h-28 mx-auto" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png">
+            <h2 class="mt-3 text-yellow-400 text-lg font-bold text-center capitalize">${personaje.name}<p class=" text-blue-300">${personaje.gender}</p></h2>
+            
+
+        </div>
+        `;
+    });
+
+};
+
+prevBtn.addEventListener('click', () => {
+
+    if(prevUrl){
+        CargarPersonajes(prevUrl);
+    }
+
+});
+
+nextBtn.addEventListener('click', () => {
+
+    if(nextUrl){
+        CargarPersonajes(nextUrl);
+    }
+
+});
+
+CargarPersonajes('https://swapi.info/api/people/');
